c0b73dac08df7130bebf929a488aaf4d41a28157
raiti-chan <rusuke@t-net.ne.jp>
2020-12-17 13:55:24 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Nekoco {
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+			this.MouseLeftButtonDown += (sender, e) => { this.DragMove(); };
+		}
+	}
+}
